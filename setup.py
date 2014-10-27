12e4458ed9ddb2a0fe40beed00083fa3cc2471c5
Adam Shimali <adam.shimali@gmail.com>
2014-10-27 21:36:03 +0000

    Added details for pypi deploy

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# see http://bugs.python.org/issue8876
+# this is just a quick hack so we can test build in vagrant
+import os
+if os.environ.get('USER','') == 'vagrant':
+  del os.link
+
 from setuptools import setup, find_packages
 
 setup(name='lrutils',
@@ -8,10 +14,16 @@ setup(name='lrutils',
       author='Land Registry',
       author_email='adam.shimali@digital.cabinet-office.gov.uk',
       url='https://github.com/LandRegistry/lr-utils',
+      download_url = 'http://github.com/LandRegistry/lr-utils/tarball/alpha',
       packages=find_packages(),
       zip_safe=False,
       include_package_data=True,
       license='MIT',
       platforms='any',
       install_requires=['passlib==1.6.2', 'py-bcrypt==0.4'],
+      classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 2.7',
+        ),
 )
