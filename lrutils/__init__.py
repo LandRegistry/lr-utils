558b8cd249470cd08a2475d2afa23f0e438462fd
treacle74 <ptrelease@gmail.com>
2014-12-15 12:38:05 +0000

    Added Errorohandler class reference to init file

--- lrutils/__init__.py
+++ lrutils/__init__.py
@@ -1,3 +1,4 @@
 from .address.address_utils import build_address
 from .password.password_utils import PasswordUtils
 from .templatefilters.template_filters import dateformat, datetimeformat, currency
+from .errorhandler.errorhandler_utils import ErrorHandler, eh_after_request
