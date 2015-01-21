8bacb39a18ab51f0c251e2f3bb9499141b691867
ptrelease <ptrelease@gmail.com>
2015-01-21 13:28:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def requirements():
         return [l.strip('\n') for l in f if l.strip('\n') and not l.startswith('#')]
 
 setup(name='lrutils',
-      version='999',
+      version='0.1.4',
       description='Utilities for LR Flask apps',
       author='Land Registry',
       author_email='paul.trelease@digital.landregistry.gov.uk',
